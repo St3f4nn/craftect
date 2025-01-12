ae28eed1a993fb4f9bf63e866867eb26b12aa839
Stefan <gajicstefan414@gmail.com>
2025-01-12 20:41:17 +0100

    Set up basic work environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.html"],
-  theme: {
-    extend: {},
+  corePlugins: {
+    container: false,
   },
-  plugins: [require("tailwindcss-debug-screens")],
+  content: ["./dist/**/*.html", "./dist/js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          // Grey color
+          950: "#0c1618",
+        },
+        yellow: {
+          // Main color
+          850: "#d1ac00",
+        },
+      },
+      fontFamily: {
+        "playfair-display": "'Playfair Display', serif",
+        outfit: "'Outfit', sans-serif",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-debug-screens"),
+
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "90rem",
+        },
+      });
+    },
+  ],
 };
