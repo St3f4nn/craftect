666838b1ecc765368192ca6ee156de1cb026b9e6
Stefan <gajicstefan414@gmail.com>
2025-01-16 20:29:14 +0100

    Create HEADER component - add mobile navigation functionality

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// Elements
+const header = document.querySelector("header");
+
+const toggleMenuBtn = document.querySelector("#toggle-menu-button");
+
+// Events
+toggleMenuBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
+});
