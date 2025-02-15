e2f28f9f165486b04237beefd3abb6991c104cce
Stefan <gajicstefan414@gmail.com>
2025-02-15 14:36:33 +0100

    Create SOLUTIONS section - make tab functionality

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,6 +7,8 @@ const toggleMenuBtn = document.querySelector("#toggle-menu-button");
 
 const yearEl = document.querySelector("#year");
 
+const tabsContainer = document.querySelector("#tabs");
+
 // FUNCTIONS
 
 // Update the current year in the footer every new year
@@ -15,6 +17,29 @@ yearEl.textContent = new Date().getFullYear();
 // EVENTS
 
 // Toggle navigation on mobile screen
-toggleMenuBtn.addEventListener("click", function () {
-  header.classList.toggle("active");
+toggleMenuBtn.addEventListener("click", () =>
+  header.classList.toggle("active")
+);
+
+// Toggle tab contents
+tabsContainer.addEventListener("click", function (e) {
+  const tab = e.target.closest("button");
+
+  if (!tab) {
+    return;
+  } else {
+    tabsContainer
+      .querySelectorAll("button")
+      .forEach(tabBtn => tabBtn.classList.remove("active-tab"));
+
+    document
+      .querySelector("#services")
+      .querySelectorAll(".service")
+      .forEach(service => service.classList.remove("active-service"));
+
+    tab.classList.add("active-tab");
+    document
+      .querySelector(`#service-${tab.dataset.tab}`)
+      .classList.add("active-service");
+  }
 });
