475b8684683c548c28426dee7995037b99de509d
Stefan <gajicstefan414@gmail.com>
2025-02-19 20:31:48 +0100

    Create STORIES section - add sliding functionality

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,10 +9,61 @@ const yearEl = document.querySelector("#year");
 
 const tabsContainer = document.querySelector("#tabs");
 
+const testimonialSlider = document.querySelector("#testimonial-slider");
+const testimonials = document.querySelectorAll(".testimonial");
+
+const arrs = document.querySelectorAll(".arr");
+const slideDots = document.querySelector("#slide-dots");
+
 // FUNCTIONS
 
 // Update the current year in the footer every new year
 yearEl.textContent = new Date().getFullYear();
+
+// Slide the slider
+const startSlidingHorizontally = (
+  slider,
+  sliderItemWidth,
+  sliderGap,
+  landmark = 0
+) => {
+  slider.style.transform = `translateX(-${
+    (sliderItemWidth + sliderGap) * landmark
+  }px)`;
+};
+
+function startSliding(slideBtn, slideLeft, slideRight) {
+  const pressedSlideBtn = btn =>
+    typeof slideBtn !== "string"
+      ? slideBtn.classList.contains(btn)
+      : slideBtn === btn;
+
+  if (pressedSlideBtn(slideRight)) {
+    sliderCounter++;
+
+    if (sliderCounter === testimonials.length) sliderCounter = 0;
+  } else if (pressedSlideBtn(slideLeft)) {
+    sliderCounter--;
+
+    if (sliderCounter < 0) sliderCounter = testimonials.length - 1;
+  }
+
+  startSlidingHorizontally(
+    testimonialSlider,
+    testimonialWidth,
+    testimonialGap,
+    sliderCounter
+  );
+
+  slideDots
+    .querySelectorAll("button.slide-dot")
+    .forEach(function (slideDot, slideDotIndex) {
+      slideDot.classList.remove("active-slide-dot");
+
+      slideDotIndex === sliderCounter &&
+        slideDot.classList.add("active-slide-dot");
+    });
+}
 
 // EVENTS
 
@@ -42,4 +93,57 @@ tabsContainer.addEventListener("click", function (e) {
       .querySelector(`#service-${tab.dataset.tab}`)
       .classList.add("active-service");
   }
+});
+
+// Sliding functionality
+const testimonialWidth = document
+  .querySelector(".testimonial")
+  .getBoundingClientRect().width;
+
+const testimonialGap = parseFloat(
+  window
+    .getComputedStyle(document.querySelector(".testimonial-gap"))
+    .getPropertyValue("gap")
+);
+
+let sliderCounter = 0;
+
+arrs.forEach(arr =>
+  arr.addEventListener("click", () =>
+    startSliding(arr, "arr-left", "arr-right")
+  )
+);
+
+slideDots.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const slideDot = e.target.closest("button.slide-dot");
+
+  if (!slideDot) {
+    return;
+  } else {
+    const slideDotIndex = Number(slideDot.dataset.slide);
+
+    slideDots
+      .querySelectorAll("button.slide-dot")
+      .forEach(slideDot => slideDot.classList.remove("active-slide-dot"));
+
+    slideDot.classList.add("active-slide-dot");
+
+    startSlidingHorizontally(
+      testimonialSlider,
+      testimonialWidth,
+      testimonialGap,
+      slideDotIndex
+    );
+
+    sliderCounter = slideDotIndex;
+  }
+});
+
+window.addEventListener("keyup", function (e) {
+  e.preventDefault();
+
+  (e.key === "ArrowLeft" || e.key === "ArrowRight") &&
+    startSliding(e.key, "ArrowLeft", "ArrowRight");
 });
